13934a66b1885bb477587f3fa73ef6f0a1653d22
Tsubasa K <mcttest0001@gmail.com>
2018-07-17 16:51:58 +0900

    add python doc.

--- entry/models.py
+++ entry/models.py
@@ -1,6 +1,10 @@
+""" module of entry app models. """
+
 from django.db import models
 
 class Entry(models.Model):
+    """ model of Entry object. """
+
     title = models.CharField(max_length=140, blank=False)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
